90cefee8d993f12acb17cbd9d86f62cdd4f68bcc
bjgaoshang <1393681849@qq.com>
2017-11-07 17:04:00 +0800

    我新创建一个类

--- app/src/main/java/com/tarena/testversion/MainActivity.java
+++ app/src/main/java/com/tarena/testversion/MainActivity.java
@@ -3,6 +3,9 @@ package com.tarena.testversion;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 我就写了点注释  咋地
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/tarena/testversion/test.java
@@ -0,0 +1,8 @@
+package com.tarena.testversion;
+
+/**
+ * 我就创建了一个类
+ */
+
+public class test {
+}
